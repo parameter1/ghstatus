ab25be5bd171c3381702eac71900a4119c5c09b3
Josh Worden <josh@parameter1.com>
2021-08-26 16:39:27 -0500

    nav links

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,9 +8,8 @@
               <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
             </div>
             <div class="ml-10 flex items-baseline space-x-4">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <router-link class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/" aria-current="page">Dashboard</router-link>
-              <router-link class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" to="/config">Config</router-link>
+              <NavLink class="px-3 py-2 rounded-md text-sm font-medium" to="/" :exact=true>Dashboard</NavLink>
+              <NavLink class="px-3 py-2 rounded-md text-sm font-medium" to="/config">Config</NavLink>
             </div>
           </div>
           <div>
@@ -29,6 +28,7 @@
 <script>
 import './css/styles.css';
 import AuthButton from './components/AuthButton.vue';
+import NavLink from './components/NavLink.vue';
 
 export default {
   name: 'App',
@@ -63,6 +63,7 @@ export default {
   },
   components: {
     AuthButton,
+    NavLink,
   },
 };
 </script>
---
+++ app/src/components/NavLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link v-bind="$props" custom v-slot="{ isActive, href, navigate }">
+    <a v-bind="$attrs" :href="href" @click="navigate" :class="isActive ? activeClass : inactiveClass" >
+      <slot />
+    </a>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'NavLink',
+  inheritAttrs: false,
+
+  props: {
+    to: String,
+    exact: {
+      type: Boolean,
+      default: false,
+    },
+    activeClass: {
+      type: String,
+      default: 'bg-gray-900 text-white',
+    },
+    inactiveClass: {
+      type: String,
+      default: 'text-gray-300 hover:bg-gray-700 hover:text-white',
+    },
+  },
+};
+</script>
